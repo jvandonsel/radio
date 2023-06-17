c99cf3f96e62ada67ee2e86199089dde73a32f0f
Jim Van Donsel <jim@jimandbarb.net>
2023-06-17 12:11:19 -0400

    Tuning improvements

--- src/tuner.ts
+++ src/tuner.ts
@@ -1,91 +1,105 @@
 import { AdcConverter } from "./adc";
 import { sleep } from "./utils";
 import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
-
-const urls = [
-    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_one.m3u8",
-    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_1xtra.m3u8",
-    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_one_dance.m3u8",
-    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_one_relax.m3u8",
-    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_two.m3u8",
-    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_three.m3u8",
-    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_fourfm.m3u8",
-    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_four_extra.m3u8",
-    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_five_live.m3u8",
-    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_6music.m3u8",
-    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_world_service.m3u8"
-];
-
+import { urls } from "./urls";
 
 export class Tuner {
 
-    adc: AdcConverter;
+    // A/D values from the left to the right side of the tuning dial
     MIN_ADC_VALUE: number = 451;
     MAX_ADC_VALUE: number = 748;
-    SLOP: number = 5;
-    WINDOW_SIZE = 20;
 
+    MPLAYER_OPTIONS = ["-loop", "0",  "-ao",  "pulse", "-really-quiet"];
+
+    // Static file
+    // TODO: Move this someplace else
+    STATIC_FILE: string = "/static1.wav";
+
+    // Width of each station on the dial in A/D values
+    WIDTH: number = 1;
+
+    // Filter window size in samples
+    FILTER_WINDOW_SIZE = 20;
+
+    adc: AdcConverter;
     adc_window: number[] = [];
     adc_to_url: Record<number, string> = {};
     radio_process: ChildProcessWithoutNullStreams | undefined = undefined;
     static_process: ChildProcessWithoutNullStreams | undefined = undefined;
+    adc_centers: number[] = [];
 
 
     public constructor() {
         this.adc = new AdcConverter();
 
-        // Map our set of URLs equally distant along the ADC range
         const num_urls = Object.keys(urls).length;
         console.log(`Found ${num_urls} URLs`);
-        const url_step = Math.floor((this.MAX_ADC_VALUE - this.MIN_ADC_VALUE) / num_urls);
 
+        // Space our URLs across the dial evenly.
+        const url_step = Math.floor((this.MAX_ADC_VALUE - this.MIN_ADC_VALUE) / num_urls);
         console.log(`step size=${url_step}`);
 
         for (let i = 0; i < num_urls; ++i) {
-            let center = url_step * i + this.MIN_ADC_VALUE;
+            const center = url_step * i + this.MIN_ADC_VALUE;
+            this.adc_centers.push(center);
 
             console.log(`Mapping center ${center} to ${urls[i]}`);
 
-            for (let k = -this.SLOP; k <= this.SLOP; ++k) {
+            // Give each station some width on the dial
+            for (let k = -this.WIDTH; k <= this.WIDTH; ++k) {
                 this.adc_to_url[center + k] = urls[i];
             }
         }
 
         // Initialize FIR filter
-        for (let i = 0; i < this.WINDOW_SIZE; ++i) {
+        for (let i = 0; i < this.FILTER_WINDOW_SIZE; ++i) {
             this.adc_window.push(0);
         }
     }
 
+    /**
+     * Start playing the given URL 
+     * @param url URL to play
+     */
     playUrl(url: string) {
-            this.radio_process = spawn('mplayer',  ["-ao", "pulse", url]);
+        this.radio_process = spawn('mplayer',  [...this.MPLAYER_OPTIONS, url], {stdio: ['pipe', 'pipe', 'pipe']});
         console.log(`Starting play process with PID ${this.radio_process.pid}`);
     }
 
-    stopUrl() {
+    /**
+     * Stop playing the current URL.
+     */
+    stopPlayingUrl() {
         if (this.radio_process) {
             console.log(`Killing play process ${this.radio_process.pid}`);
-            this.radio_process.kill('SIGKILL');
+            this.radio_process.stdio[0].write('q');
         }
         this.radio_process = undefined;
     }
 
+    /**
+     * Stop all sound
+     */
     stopAll() {
-        this.stopUrl();
+        this.stopPlayingUrl();
         this.killStatic();
     }
 
-
-    playStatic() {
+    /**
+     * Resume playing static, or start playing static if we haven't yet.
+     */
+    resumeStatic() {
         if (!this.static_process) {
-            const file_loc = "/static1.wav";
-            this.static_process = spawn('mplayer', ["-loop", "0",  "-ao",  "pulse", file_loc]);
+            this.static_process = spawn('mplayer', [...this.MPLAYER_OPTIONS, this.STATIC_FILE]);
         } else {
             console.log("Unpausing static");
             this.static_process.kill('SIGCONT');
         }
     }
 
+    /**
+     * Pause playing static.
+     */
     pauseStatic(){
         if (this.static_process) {
             console.log("Pausing static");
@@ -100,44 +114,86 @@ export class Tuner {
         }
     }
 
+    /**
+     * Read a raw value from the ADC, filter it, and return a filtered value.
+     * @returns A filtered ADC value.
+     */
+    getFilteredAdcValue(): number {
+        // Read a tuning value from the ADC
+        const raw_value: number = this.adc.readAdc();
 
+        // Add to sliding window filter and get the filtered value
+        this.adc_window.push(raw_value);
+        this.adc_window.shift();
+        const window_sum = this.adc_window.reduce((a, b) => a + b, 0);
+        const filtered_value = Math.floor(window_sum / this.FILTER_WINDOW_SIZE);
+
+        //console.log(`ADC: ${raw_value}, filtered: ${filtered_value}`);
+
+        return filtered_value;
+    }
+
+    /**
+     * Find the nearest center ADC value to the given ADC value.
+     * @param adc_value 
+     */
+    findNearestCenter(adc_value: number): number {
+        // TODO: cache these?
+        return this.adc_centers.reduce((a, b) => Math.abs(b - adc_value) < Math.abs(a - adc_value) ? b : a);
+    }
+
+    /**
+     * Continuously monitor the tuning ADC and play the appropriate station.
+     * Never returns.
+     */
     public async tune() {
 
-        var last_adc_value = 0;
-        const tuning_hysteresis = 3;
+        var is_locked = false;
+        var locked_center = 0;
+
+        // How close we need to be to a station center to lock onto it
+        const PULL_IN_THRESHOLD = 5;
+        // How far we need to wander from the center of a locked station to unlock from it
+        const PULL_OFF_THRESHOLD = 8;
+
+        // Start playing static
+        this.resumeStatic();
+
         while (true) {
+            await sleep(100);
+
             try {
-                // Read a tuning value
-                const adc_value: number = this.adc.readAdc();
-
-                // Add to sliding window filter and get the filtered value
-                this.adc_window.push(adc_value);
-                this.adc_window.shift();
-                const window_sum = this.adc_window.reduce((a, b) => a + b, 0);
-                const filtered_adc_value = Math.floor(window_sum / this.WINDOW_SIZE);
-
-                console.log(`ADC: ${adc_value}, filtered: ${filtered_adc_value} last=${last_adc_value}`);
+                const filtered_adc_value = this.getFilteredAdcValue();
                 
-                if (Math.abs(filtered_adc_value - last_adc_value) > tuning_hysteresis) {
-                    if (filtered_adc_value in this.adc_to_url) {
-                        const url = this.adc_to_url[filtered_adc_value];
-                        console.log(`Playing: ${url}`);
+                if (is_locked) {
+                    // Currently Locked
+                    if (Math.abs(filtered_adc_value - locked_center) > PULL_OFF_THRESHOLD) {
+                        console.log("Unlocking");
+                        is_locked = false;
+                        this.stopPlayingUrl()
+                        this.resumeStatic()
+                    } else {
+                        // Still locked. Keep playing.
+                    }
+                } else {
+                    // Currently not locked. Check if we should lock on a station.
+                    // Find the nearest center ADC value and if it's close enough lock onto it.
+                    const nearest_center = this.findNearestCenter(filtered_adc_value);  
+                    if (Math.abs(nearest_center - filtered_adc_value) <= PULL_IN_THRESHOLD) {
+                        const url = this.adc_to_url[nearest_center];
+                        console.log(`Playing ${nearest_center} ${url}`);
+                        locked_center = nearest_center;
+                        is_locked = true;
                         this.pauseStatic();
-                        this.stopUrl();
+                        this.stopPlayingUrl();
                         this.playUrl(url);
                     } else {
-                        console.log("Playing static");
-                        this.stopUrl()
-                        this.playStatic()
+                        // Stay unlocked
                     }
-                    last_adc_value = filtered_adc_value;
                 }
-
             } catch (e) {
                 console.error(`Caught error reading ADC: ${e}\n`);
             }
-
-            await sleep(100);
         }
     }
 }
---
+++ src/urls.ts
@@ -0,0 +1,15 @@
+
+export const urls = [
+    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_one.m3u8",
+    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_1xtra.m3u8",
+    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_one_dance.m3u8",
+    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_one_relax.m3u8",
+    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_two.m3u8",
+    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_three.m3u8",
+    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_fourfm.m3u8",
+    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_four_extra.m3u8",
+    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_five_live.m3u8",
+    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_6music.m3u8",
+    "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_world_service.m3u8"
+];
+
